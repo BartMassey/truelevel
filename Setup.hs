d4d229185458b4bc5cfafe716576f2e786b7f4bf
bart <bart@cs.pdx.edu>
2008-01-21 03:14:10 -0800

    fixed style bug in WAVE; deleted perhaps-obsolete TODO
    darcs-hash:20080121111410-666ac-d69143f5ae1a19289f945bebdbb4e218e7077bc2

--- Setup.hs
+++
@@ -1,7 +0,0 @@
---- Copyright (C) 2007 Bart Massey
---- ALL RIGHTS RESERVED
---- Please see the file COPYING in this software
---- distribution for license information.
-
-import Distribution.Simple
-main = defaultMain
